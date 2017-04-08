97c3a3055118b24f4da640a41ff1cb88329fac41
lionel lints <lionel.lints@gmail.com>
2017-04-08 08:46:07 -0700

    add basic user routes 👷

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+
+const games = require('./games');
 const tables = require('../../db/tables');
 
 /* GET users. */
@@ -11,5 +13,23 @@ router.get('/', (req, res) => {
   });
 });
 
+/* GET a user. */
+router.get('/:id', (req, res, next) => {
+});
+
+/* POST a new user. */
+router.post('/', (req, res, next) => {
+});
+
+/* UPDATE a user. */
+router.put('/:id', (req, res, next) => {
+});
+
+/* Delete a user. */
+router.delete('/:id', (req, res, next) => {
+});
+
+/* Add the games resource for a user. */
+router.use('/:user_id/games', games);
 
 module.exports = router;
