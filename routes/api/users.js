b978c986a0aafddbf8066b8c1d7875529154c9f0
lionel lints <lionel.lints@gmail.com>
2017-04-12 10:38:36 -0700

    update route updated (linted, small fixes) 🗑

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const moment = require('moment');
 const games = require('./games');
 const tables = require('../../db/tables');
 
@@ -23,19 +24,23 @@ router.put('/:id', (req, res, next) => {
     const returnArray = ['id', 'updated_at'];
     const keyArray = Object.keys(req.body).filter(key => key !== 'id');
 
-    for (const key of keyArray) {
+    /* Itterate through the keys, add them to the new object and array */
+    keyArray.forEach((key) => {
       updatedUser[key] = req.body[key];
       returnArray.push(key);
-    }
+    });
 
+    /* Disallow updates of the id field */
     if (req.body.id !== Number(req.user.id)) {
       res.status(422).json({ error: 'You cannot update the id field.' });
-    } 
-    updatedUser['updated_at'] = moment();
+    }
+
+    updatedUser.updated_at = moment();
     tables.Users().returning(returnArray).where('id', req.user.id).update(updatedUser)
     .then((returnedUser) => {
       res.json(returnedUser);
-    }).catch((error) => {
+    })
+    .catch((error) => {
       res.status(500).json({
         status: 'user not updated',
         error,
@@ -48,10 +53,11 @@ router.put('/:id', (req, res, next) => {
 
 /* Delete a user. */
 router.delete('/:id', (req, res, next) => {
-  if (res.locals.user.id === req.params.id){
-    tables.Users().where('id', req.user.id).del().then((user) =>{
+  if (res.locals.user.id === req.params.id) {
+    tables.Users().where('id', req.user.id).del().then((user) => {
       res.sendStatus(204);
-    }).catch((error) => {
+    })
+    .catch((error) => {
       res.status(500).json({
         status: 'user not removed from db',
         error,
