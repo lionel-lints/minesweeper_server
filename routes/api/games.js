5419d99aff97ed74f92a0ff01b45b8922dc9a1e8
lionel lints <lionel.lints@gmail.com>
2017-04-12 13:12:00 -0700

    add post game route 📬

--- routes/api/games.js
+++ routes/api/games.js
@@ -5,7 +5,7 @@ const router = express.Router({ mergeParams: true });
 
 /* GET all user's games. */
 router.get('/', (req, res) => {
-  if (res.locals.user.id === req.params.user_id) {
+  if (res.locals.user.sub === Number(req.params.user_id)) {
     tables.Games().where({ user_id: req.params.user_id }).then((games) => {
       res.json(games);
     }).catch((error) => {
@@ -21,7 +21,7 @@ router.get('/', (req, res) => {
 
 /* DELETE all user's games. */
 router.delete('/', (req, res, next) => {
-  if (res.locals.user.id === req.params.user_id) {
+  if (res.locals.user.sub === Number(req.params.user_id)) {
     tables.Games().where('id', req.user.id).del().then((response) => {
       res.sendStatus(204);
     })
@@ -38,6 +38,30 @@ router.delete('/', (req, res, next) => {
 
 /* POST a new game. */
 router.post('/', (req, res, next) => {
+  if (res.locals.user.sub === Number(req.params.user_id)) {
+    tables.Games().insert({
+      created_at: moment(),
+      updated_at: moment(),
+      user_id: req.user.id,
+      completion_speed: req.body.speed,
+      current_game: req.body.current_game,
+      successfully_completed: req.body.won,
+    }).returning('*')
+    .then((newGame) => {
+      res.status(200).json({
+        status: 'success',
+        newGame,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        status: 'game not added to db',
+        error,
+      });
+    });
+  } else {
+    res.status(401).json({ message: 'UnAuthorized' });
+  }
 });
 
 /* DELETE a game. */
